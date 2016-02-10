ca324fc84f8ca24d0219ede61c456e791d44ea60
valmsmith39a <valmsmith39a@gmail.com>
2016-02-09 17:36:08 -0800

    Set up service and method for finding stock

--- main.js
+++ main.js
@@ -10,18 +10,18 @@ app.config(function($stateProvider, $urlRouterProvider) {
   $urlRouterProvider.otherwise('/'); 
 });
 
-app.controller('addStocksCtrl', function($scope, $state) {
+app.controller('addStocksCtrl', function($scope, $state, FindStock) {
   console.log('in add stocksCtrl');
   $scope.findStock = function(){
     console.log('Find stock');
     console.log('$state', $state);
-    console.log('new stock quote: ', $scope.newStock.quote);
-    // Call service 
-    $scope.stocks = [{quote:'aapl', name:'Apple', closingPrice:'782.23'}, {quote:'msft', name:'Microsoft', closingPrice:'72.90'}, {quote:'wmt', name:'Walmart', closingPrice:'43.23'}];
-    // $state.go('home');
-  };
-});
+    FindStock.find($scope);
 
+    
+  };
+})
+
+/*
 app.controller('listStocksCtrl', function($scope, $state) {
   console.log('list stocks Ctrl');
   $scope.doAThing = function(){
@@ -30,6 +30,39 @@ app.controller('listStocksCtrl', function($scope, $state) {
     $state.go('home');
   };
 });
+*/
+
+app.service('FindStock', function() {
+  this.find = function($scope) {
+    console.log('inside FindStock this.find()');
+  
+    debugger;
+    console.log('stock quote is: ', ($scope.newStock.quote));
+
+  } 
+  /*
+  $localStorage.$default({
+    potions:[]
+  });
+  this.list = potions;
+  this.list = [
+    {name:'growth', color:'green', cost:20.00},
+    {name:'speed', color:'red', cost:30.00},
+    {name:'happy', color:'blue', cost:40.00},
+    {name:'energy', color:'yellow', cost:60.00, fruit:'strawberries'}
+  ];
+  this.add = function(potion) {
+    this.list.push(potion);
+  }
+  this.remove = function(potion) {
+    var index = this.list.indexOf(potion);
+    this.list.splice(index, 1);
+  }
+  */
+});
+
+
+
 /*
 app.controller('testCtrl', function($scope, $http) {
   console.log('testCtrl');
